61eac33f0a26d7bff50b3b4547b186c064daafa2
Alekseev Oleg <01.wftp@gmail.com>
2023-08-01 12:58:26 +0300

    Make reminder list with checkmark button

---
+++ Today/ListViewController/ReminderDoneButton.swift
@@ -0,0 +1,12 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Олег Алексеев on 01.08.2023.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+}
